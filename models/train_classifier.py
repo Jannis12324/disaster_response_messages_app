74a2967cf202810c38483f2ab432ce3d0021ec0a
Jannis Jehmlich <jannis.j@hey.com>
2020-10-29 20:08:22 +0100

    feat: adds train classifier script to train and save the model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,97 @@
+# import libraries
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+nltk.download(['punkt', 'wordnet'])
+
+# TODO: funktionsbeschreibungen ergänzen
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Loads the dataframe from the database with the messages and labeled categories.
+    :param database_filepath: (string) path to database file
+    :return: X: messages, Y: labeled categories, category names
+    """
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql("SELECT * FROM messages", engine, index_col="id")
+    X = df["message"]
+    Y = df.iloc[:, 3:]
+
+    return X, Y, Y.columns
 
 
 def tokenize(text):
-    pass
+    """
+    Gets handed a string and returns it as lemmatized tokens.
+    :param text: (string) one message, of the messages
+    :return: message as list of tokens
+    """
+    finished_tokens = []
+    tokens = word_tokenize(text)
+    lemma = WordNetLemmatizer()
+
+    for token in tokens:
+        processed_token = lemma.lemmatize(token).lower().strip()
+        finished_tokens.append(processed_token)
+
+    return finished_tokens
 
 
 def build_model():
-    pass
+    """
+
+    :return:
+    """
+    # TODO: parameter für das modell einfügen
+    pipeline = Pipeline([
+        ("vect", CountVectorizer(tokenizer=tokenize)),
+        ("tfidf", TfidfTransformer()),
+        ("moclf", MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+
+    :param model:
+    :param X_test:
+    :param Y_test:
+    :param category_names:
+    :return:
+    """
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+
+    :param model:
+    :param model_filepath:
+    :return:
+    """
+    # Dump the trained decision tree classifier with Pickle
+    model_filename = model_filepath
+    # Open the file to save as pkl file
+    cv_model_pkl = open(model_filename, 'wb')
+    pickle.dump(model, cv_model_pkl)
+    # Close the pickle instances
+    cv_model_pkl.close()
 
 
 def main():
