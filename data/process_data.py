7ebc9b1747c88d7830a76a2075226910e00508be
Jannis Jehmlich <jannis.j@hey.com>
2020-10-29 18:01:54 +0100

    feat: completes first version of process_data.py to load, transform and save the messages

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,60 @@
+#import statements
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Loads the messages and categories from csv and merges them
+    :param messages_filepath: (string) filepath to messages csv
+    :param categories_filepath: (string) filepath to categories csv
+    :return: (pandas dataframe) merged dataframe
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    # merge the datasets
+    df = pd.merge(messages, categories, on="id")
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Expands the message categories to one column per category. Cleans the data so the category rows only contain
+    1 or 0. Removes duplicates.
+    :param df: (pandas dataframe) The loaded and merges dataframe of messages and categories
+    :return: (pandas dataframe) Cleaned and transformed
+    """
+    # creates a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(";", expand=True)
+    # selects the first row of the categories dataframe
+    row = categories.iloc[0]
+    # extract the column names
+    category_colnames = row.apply(lambda x: x[:-2])
+    # renames the columns of `categories`
+    categories.columns = category_colnames
+
+    # iterates through columns to reduce the entries to 1 or 0
+    for column in categories:
+        # sets each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
+
+        # converts column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drops the original categories column from `df`
+    df.drop(columns="categories", inplace=True)
+    # concatenates the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # drops duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('messages', engine, index=False)
 
 
 def main():
